59b19a38d630300c8b8ea053b8d985b27dbe0d4f
agis <agis.septiyanto@gmail.com>
2021-03-03 07:30:40 +0700

    add function to submit inline comment review

--- foreground.js
+++ foreground.js
@@ -51,6 +51,25 @@ if (url.includes('file://')) {
     }
 }
 
+if (url.includes('https://www.dicoding.com/academysubmissions') && url.endsWith('review')) {
+    document.querySelector('.js-source-code').addEventListener('click', () => {
+        const commentForm = document.querySelector('.js-line-comment .js-comment-form')
+        if (commentForm !== null) {
+            commentForm.addEventListener("keydown", function (e) {
+                if ((e.which == 13 || e.which == 10)) {
+                    if (e.ctrlKey) {
+                        e.stopImmediatePropagation();
+                        e.preventDefault();
+                        document.querySelector('.js-comment-form .btn-primary').click()
+                    }
+                }
+            }, true);
+        }
+    })
+
+}
+
+
 if (url.includes('https://www.dicoding.com/dashboard')) {
     const table = document.querySelector('.student-satisfaction-stats')
     // create an observer instance
